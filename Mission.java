36ac3bd04b668662bc9896085e74c9c4e3d9f0a8
carlitoaster <caelan30@Gmail.com>
2025-05-03 23:40:56 +0800

    added java mission file and class fields

---
+++ Mission.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Mission{
+    //class fields
+    private String missionName;
+    private String missionCode;
+    private String destinationPlanet;
+    private Int launchYear;
+    private Double successRate;
+    private Boolean mannedMission;
+
+}
